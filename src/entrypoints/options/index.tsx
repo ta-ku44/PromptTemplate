2fc33273b3e740567bc8e2af4f5f4c04c8b7bff4
ta-ku44 <213548904+ta-ku44@users.noreply.github.com>
2026-02-23 20:34:27 +0900

    fix: correct import case for OptionsApp and PopupApp components

--- src/entrypoints/options/index.tsx
+++ src/entrypoints/options/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@/assets/tailwind.css';
 
-import OptionsApp from './optionsApp';
+import OptionsApp from './OptionsApp';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/entrypoints/popup/index.tsx
+++ src/entrypoints/popup/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@/assets/tailwind.css';
 
-import PopupApp from './popupApp';
+import PopupApp from './PopupApp';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
